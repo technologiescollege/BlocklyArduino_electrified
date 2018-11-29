d69d8eac1c66e82baebd70deaae82166078c935c
SebCanet <scanet@libreduc.cc>
2018-11-29 15:06:27 +0100

    update B@ version number

--- resources/app/main.js
+++ resources/app/main.js
@@ -75,7 +75,7 @@ function createWindow () {
 		// })
 	}
 	mainWindow.setMenu(null);
-	// mainWindow.webContents.openDevTools();
+	mainWindow.webContents.openDevTools();
 	mainWindow.on('closed', function () {
 		mainWindow = null
 	})
